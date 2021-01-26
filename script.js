0906dd03fd519493af121d55bfca3772dacf1c38
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2021-01-27 03:29:47 +0530

    Pratik-Sarkate completed task-2

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web Designer","Programmer","Learning Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
